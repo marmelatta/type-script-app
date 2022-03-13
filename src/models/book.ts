864d7d5781e33cd0c1cb6a11a1cf336eeff0c7af
marmelatta <testandroidapp@yandex.ru>
2022-03-13 23:25:52 +0500

    homework

---
+++ src/models/book.ts
@@ -0,0 +1,10 @@
+interface Book {
+    id: number;
+    title: string,
+    description: string,
+    authors: string,
+    favorite: string,
+    fileCover: string,
+    fileName: string,
+    fileBook: string,
+}
---
+++ src/repositories/books-repository.ts
@@ -0,0 +1,17 @@
+class BooksRepository {
+    createBook(book: Book) {
+    } //создание книги
+    getBook(id: number)
+    {
+        
+    } // - получение книги по id
+    getBooks(){
+        
+    } // - получение всех книг
+    updateBook(id: number){
+        
+    } //- обновление книги
+    deleteBook(id: number){
+        
+    } //- удаление книги
+}
